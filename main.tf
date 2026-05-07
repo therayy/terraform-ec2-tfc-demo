41e740d238615d01cfb01fadeccabd6973990d22
Raafat Abaid <82475946+therayy@users.noreply.github.com>
2026-05-07 14:52:54 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,15 @@
-provider "aws" {
-  region = var.aws_region
-}
-
 resource "aws_instance" "lesson_03" {
   ami           = "ami-0e68dc81dc36750a1"
   instance_type = var.instance_type
 
-  subnet_id              = "subnet-0637c661ee87c4e9e"
-  vpc_security_group_ids = ["sg-01f98fefb69bc5320"]
-
+  subnet_id                   = "subnet-0637c661ee87c4e9e"
+  vpc_security_group_ids      = ["sg-01f98fefb69bc5320"]
   associate_public_ip_address = true
 
   tags = {
-    Name = "my-ec2-instance"
+    Name        = "my-ec2-instance"
+    Owner       = "ray.abaid@hashicorp.com"
+    Environment = "test"
+    Project     = "terraform-learning"
   }
 }
