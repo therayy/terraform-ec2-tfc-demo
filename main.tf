0748c634d7da5fb95dbdf2fa91ec112de8d2be69
Raafat Abaid <ray@Raafats-MBP.lan>
2026-05-07 14:14:45 -0700

    add subnet and security group

--- main.tf
+++ main.tf
@@ -3,8 +3,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "lesson_03" {
-  ami           = "ami-0b671272c81662a99"
+  ami           = "ami-0e68dc81dc36750a1"
   instance_type = var.instance_type
+
+  subnet_id              = "subnet-PASTE-HERE"
+  vpc_security_group_ids = ["sg-01f98efb69bc5320"]
+
+  associate_public_ip_address = true
 
   tags = {
     Name = "my-ec2-instance"
