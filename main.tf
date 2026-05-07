81e6de78bdb9d9e9570df6f2d0893de58afc2a88
Raafat Abaid <82475946+therayy@users.noreply.github.com>
2026-05-07 14:28:55 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "lesson_03" {
   ami           = "ami-0e68dc81dc36750a1"
   instance_type = var.instance_type
 
-  subnet_id              = "subnet-PASTE-HERE"
+  subnet_id              = "subnet-0637c661ee87c4e9e"
   vpc_security_group_ids = ["sg-01f98efb69bc5320"]
 
   associate_public_ip_address = true
