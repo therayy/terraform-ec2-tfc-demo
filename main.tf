38267e9a636e47bd774d1dce6a2fec76a9ce635d
Raafat Abaid <82475946+therayy@users.noreply.github.com>
2026-05-07 15:48:03 -0700

    Refactor AWS instance resource configuration
    Removed subnet ID, security group IDs, and public IP association from AWS instance resource.

--- main.tf
+++ main.tf
@@ -6,10 +6,6 @@ resource "aws_instance" "lesson_03" {
   ami           = "ami-0b671272c81662a99"
   instance_type = var.instance_type
 
-  subnet_id                   = "subnet-0637c661ee87c4e9e"
-  vpc_security_group_ids      = ["sg-01f98fefb69bc5320"]
-  associate_public_ip_address = true
-
   tags = {
     Name        = "my-ec2-instance"
     Owner       = "ray.abaid@hashicorp.com"
